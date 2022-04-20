41c785eb227966181c4fc92cef30b32c7f71bd56
blain <blain@argalladas.eu>
2022-04-20 04:20:48 +0200

    Add integration test with cypress

---
+++ cypress/integration/dashboard.spec.js
@@ -0,0 +1,14 @@
+describe('Modify params in dashboard', () => {
+  it('The dashboard apply filter and change url with the params', () => {
+    cy.visit('/demo/index.html')
+    cy.get('#tpep_pickup_datetime')
+      .type('2017-01-18')
+    cy.get('#tpep_pickup_datetime_btn').click()
+    cy.url().should('include', '?tpep_pickup_datetime=2017-01-18&filters=toDate%28tpep_pickup_datetime%29+%3D+%272017-01-18%27')
+  })
+
+  it('In the dashboard layout should exist 4 tables', () => {
+    cy.visit('/demo/index.html')
+    cy.get('.tinytable').should('have.length', 4)
+  })
+})
